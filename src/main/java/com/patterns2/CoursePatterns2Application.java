3c1146f4c2747efea82800d2121d8fd3c351b2c3
sionziom <adriansionek95@gmail\.com>
2020-07-20 20:38:15 +0200

    first commit

---
+++ src/main/java/com/patterns2/CoursePatterns2Application.java
@@ -0,0 +1,13 @@
+package com.patterns2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePatterns2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursePatterns2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/patterns2/CoursePatterns2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patterns2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePatterns2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

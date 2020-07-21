6138307583ec0dbb0263e716a690b44bcc1be3b6
sionziom <adriansionek95@gmail\.com>
2020-07-21 21:41:15 +0200

    aspect

--- src/main/java/com/patterns2/facade/api/OrderFacade.java
+++ src/main/java/com/patterns2/facade/api/OrderFacade.java
@@ -3,12 +3,14 @@ package com.patterns2.facade.api;
 import com.patterns2.facade.ShopService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
-public final class OrderFacade {
+@EnableAspectJAutoProxy
+public class OrderFacade {
 
     private ShopService shopService;
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
